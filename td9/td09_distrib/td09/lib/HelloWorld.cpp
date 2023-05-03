e215e6b68b5c1e9892a895b693e6a28f62c22880
MichelK <michel.kadilar@etu.unice.fr>
2023-05-03 13:56:58 +0200

    TD9

---
+++ td9/td09_distrib/td09/lib/HelloWorld.cpp
@@ -0,0 +1,67 @@
+/*
+ * Mon formatter a bugué lors de ce TP, il n'y a donc pas de formatage du code :)
+ * Il reste plutôt lisible...
+ */
+
+#include <stdio.h>
+#include "HelloWorld.h"
+
+JNIEXPORT void JNICALL Java_HelloWorld_printCpp(JNIEnv *, jclass) {
+printf("World !\n");
+}
+
+JNIEXPORT void JNICALL Java_HelloWorld_printStringToCpp(JNIEnv *env, jclass cl, jstring str)
+{
+// Construction d'une chaîne C/C++ à partir d'une chaîne Java
+const char *cStr = env->GetStringUTFChars(str,0);
+// Affichage de la chaîne C++
+printf("%s\n", cStr);
+// Libération de la chaîne C++
+env->ReleaseStringUTFChars(str,cStr);
+}
+JNIEXPORT jstring JNICALL Java_HelloWorld_stringFromCpp(JNIEnv *env, jobject obj)
+{
+// Construction d'une chaîne Java à partir d'une chaîne C/C++
+return env->NewStringUTF("Chaîne en C");
+}
+
+JNIEXPORT void JNICALL Java_HelloWorld_callJavaMethod(JNIEnv *env, jobject obj) {
+// Récupération d'un objet de Metaclasse
+jclass cls = env->GetObjectClass(obj);
+// Calcule de l'identificateur de "void test(String str)"
+jmethodID mid = env->GetMethodID(cls,"test","(Ljava/lang/String;)V");
+if (mid == 0) {
+// Ca a planté !!!
+fprintf(stderr, "Ouille, ça a planté !");
+} else {
+// Tout va bien, l'appel peut aboutir.
+jstring str = env->NewStringUTF("Ceci est un paramètre créé en C/C++");
+env->CallVoidMethod(obj,mid,str);
+}
+return;
+}
+
+JNIEXPORT jstring JNICALL Java_HelloWorld_toString(JNIEnv *env, jobject obj) {
+char buffer[256];
+// Obtention de la Metaclasse de HelloWorld
+jclass cls = env->GetObjectClass(obj);
+// Calcul de l'identificateur de l'attribut entier de type int
+jfieldID fid = env->GetFieldID(cls,"entier","I");
+// Récupération de la valeur entière de l'attribut
+int a = env->GetIntField(obj,fid);
+// Modification de la valeur entière de l'attribut
+env->SetIntField(obj, fid, a + 1);
+// Deuxieme récupération de la valeur entière de l'attribut
+a = env -> GetIntField(obj,fid);
+// Génération d'une chaîne contenant la valeur de l'attribut
+sprintf(buffer,"Hello [a = %d]",a);
+// On retourne un objet Java de chaîne de caractères
+return env->NewStringUTF(buffer);
+}
+
+JNIEXPORT int JNICALL Java_HelloWorld_fib(JNIEnv *env, jclass cl, jint n){
+    if (n < 2)
+        return n;
+    else
+        return Java_HelloWorld_fib(env, cl, n-1) + Java_HelloWorld_fib(env, cl, n-2);
+}
