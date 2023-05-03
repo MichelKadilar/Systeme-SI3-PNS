c937c096453e40a91edbdc963b86f3e700d00eb5
MichelK <michel.kadilar@etu.unice.fr>
2023-05-03 14:01:37 +0200

    TD9

---
+++ td9/td09_distrib/td09/HelloWorld.java
@@ -0,0 +1,47 @@
+/**
+ * Created by lavirott on 13/05/2017, modified 27/04/2023.
+ */
+import java.util.Scanner;
+
+public class HelloWorld {
+
+    private int entier; // on aurait pu le rendre public et statique et donc avoir directement accès à la variable dans
+    // le main
+
+    // Méthodes natives fournies grâce à une bibliothèque
+    public static native void printCpp();
+    public static native void printStringToCpp(String str);
+    public native String stringFromCpp();
+    public native void callJavaMethod();
+    public native String toString();
+    public static native int fib(int n);
+
+    static {
+        System.out.print("Loading Hello World native library...");
+        System.loadLibrary("HelloWorld");
+        System.out.println("done.");
+    }
+
+    public static void main(String args[]) {
+        HelloWorld hw = new HelloWorld();
+        // Print from Java and from C/C++
+        System.out.print("Hello ");
+        HelloWorld.printCpp();
+        HelloWorld.printStringToCpp("toto");
+        String strFromCpp = hw.stringFromCpp();
+        System.out.println(strFromCpp);
+        hw.callJavaMethod();
+        hw.entier = 5;
+        System.out.println(hw.toString());
+        System.out.println("Fibonnaci result for fib(" + hw.entier + ") : " + HelloWorld.fib(6));
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Veuillez appuyer sur Entrée pour terminer le programme...");
+        sc.nextLine();
+        sc.close();
+    }
+
+    public void test(String string){
+        System.out.println(string);
+    }
+}
